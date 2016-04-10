4bcd8a114138475345488063e26a4652c65b2788
Willow Bumby <willow.rose@bumby.ca>
2016-04-09 17:16:16 -0700

    Add starter project

---
+++ Plaid/Plaid.h
@@ -0,0 +1,19 @@
+//
+//  Plaid.h
+//  Plaid
+//
+//  Created by Willow Bumby on 2016-04-09.
+//  Copyright © 2016 Willow Bumby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Plaid.
+FOUNDATION_EXPORT double PlaidVersionNumber;
+
+//! Project version string for Plaid.
+FOUNDATION_EXPORT const unsigned char PlaidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plaid/PublicHeader.h>
+
+
