03d83184b862e4ba4f7edf325e2527fc26f6aa6f
Willow Bumby <willow.rose@bumby.ca>
2016-04-10 21:46:39 -0700

    Add tests for PlaidClient.swift

--- Plaid/PlaidConstants.swift
+++ Plaid/PlaidConstants.swift
@@ -6,13 +6,13 @@
 //  Copyright © 2016 Willow Bumby. All rights reserved.
 //
 
-struct PlaidConstants {
+public struct PlaidConstants {
     
     // Development and Testing
-    internal static let TestSecret = "test_secret"
-    internal static let TestID = "test_id"
-    internal static let TestUsername = "plaid_test"
-    internal static let TestPassword = "plaid_test"
+    public static let TestSecret = "test_secret"
+    public static let TestClientID = "test_id"
+    public static let TestUsername = "plaid_test"
+    public static let TestPassword = "plaid_test"
     
     // API Hosts
     internal static let DevelopmentAPIHost = "https://tartan.plaid.com/"
