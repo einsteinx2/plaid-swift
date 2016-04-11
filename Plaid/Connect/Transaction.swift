ebcc62fe1f1ee07f955dc4213fe67e2fe907ab91
Willow Bumby <willow.rose@bumby.ca>
2016-04-10 23:43:46 -0700

    Add TransactionElementsTrait and TransactionInitializationTrait protocols

--- Plaid/Connect/Transaction.swift
+++ Plaid/Connect/Transaction.swift
@@ -8,6 +8,42 @@
 
 import Foundation
 
+private protocol TransactionElementsTrait {
+    
+    var id: String { get }
+    var account: String { get }
+    
+    var name: String? { get }
+    var amount: Float? { get }
+    var date: NSDate? { get }
+    
+    var meta: [String: NSObject]? { get }
+    var address: String? { get }
+    var city: String? { get }
+    var state: String? { get }
+    var zip: String? { get }
+    var longitude: Double? { get }
+    var latitude: Double? { get }
+    
+    var pending: Bool? { get }
+    var type: [String: NSObject]? { get }
+    var category: [String: [String]]? { get }
+    var categoryID: String { get }
+    
+    var score: [String: NSObject] { get }
+    var stateScore: Float? { get }
+    var cityScore: Float? { get }
+    var addressScore: Float? { get }
+    
+    var referenceNumber: Int { get }
+    var pendingTransaction: Bool { get }
+    
+}
+
+private protocol TransactionInitializationTrait {
+    init(transaction: [String: NSObject])
+}
+
 public struct Transaction {
     
     
